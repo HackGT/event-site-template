ec8b1b12aac84bd8c6cd6e9b297fe445c721f364
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-06-13 12:12:59 -0400

    Revert "Merge branch 'main' of https://github.com/HackGT/event-site-template into main"
    This reverts commit 89d8753e78aa164067db19dcc3211f899e9ee913, reversing
    changes made to 10200f9e7caf2aa2b898820a7299c7607fab9a93.

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,27 +1,9 @@
-import { Text, VStack } from "@chakra-ui/react";
-import styles from "./About.module.scss"
+import { Text } from "@chakra-ui/react";
+
 const About = () => {
   return (
-    <div className={styles.container}>
-      <VStack>
-      <Text className={styles.mainText}>About</Text>
-      <Text className={styles.bodyText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna 
-         aliqua. Mauris cursus mattis molestie a iaculis at erat. 
-         Semper viverra nam libero justo laoreet sit amet cursus sit. 
-         Viverra tellus in hac habitasse platea dictumst vestibulum 
-         rhoncus. Posuere lorem ipsum dolor sit amet consectetur.
-        Sed blandit libero volutpat sed.</Text>
-
-      <Text className={styles.mainText}>What is HackGT?</Text>
-      <Text className={styles.bodyText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna 
-         aliqua. Mauris cursus mattis molestie a iaculis at erat. 
-         Semper viverra nam libero justo laoreet sit amet cursus sit. 
-         Viverra tellus in hac habitasse platea dictumst vestibulum 
-         rhoncus. Posuere lorem ipsum dolor sit amet consectetur.
-        Sed blandit libero volutpat sed.</Text>
-      </VStack>
+    <div>
+      <Text>Where the about should be</Text>
     </div>
   );
 };
